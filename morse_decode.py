2ba29f3842f5a1a2bed1fcda896b29556dd2e82c
Ezjin <pmartins@duck.com>
2024-08-13 15:38:16 -0300

    funcao morse_code_function, pega uma string e transforma em codigo morse

--- morse_decode.py
+++ morse_decode.py
@@ -1,10 +1,26 @@
 import pandas as pd
+from dotenv import main
+import os
+import ast
 
+# Load the .env file
 
-text = 'tem tres palavras'
+main.load_dotenv(dotenv_path=".env")
 
-words = text.split()
+# Get the MORSE_CODE variable and convert it back to a dictionary
+morse_code_str = os.getenv("MORSE_CODE")
+morse_code = ast.literal_eval(morse_code_str)
 
-for word in words:
-    for l in word:
-        print(l)
+def morse_code_function(msg, dict):
+    
+    words = msg.split()
+    code_msg = []
+    for word in words:
+        morse_word = []
+        for letter in word:
+            morse_word.append(dict[letter.upper()])
+            word_code = " ".join(morse_word)
+        code_msg.append(word_code)
+    
+    return "  ".join(code_msg)
+
